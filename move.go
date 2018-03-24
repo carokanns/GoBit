7772042edbd530f97b380694261b2aae6cda45c7
caro kann <jp@fendrich.se>
2018-03-24 17:35:46 +0100

    handle go command in fenstring

--- move.go
+++ move.go
@@ -13,9 +13,7 @@ const (
 )
 
 var pieceRules [nP][]int // not pawns
-type moveList struct {
-	mv []move
-}
+
 func init() {
 	pieceRules[Rook] = append(pieceRules[Rook], E)
 	pieceRules[Rook] = append(pieceRules[Rook], W)
@@ -23,10 +21,25 @@ func init() {
 	pieceRules[Rook] = append(pieceRules[Rook], S)
 }
 
-func (ml *moveList) add(mv move) {
-	ml.mv = append(ml.mv, mv)
+type move uint64
+type moveList []move
+func (mvs *moveList) add(mv move) {
+	*mvs = append(*mvs, mv)
 }
 
-type move uint64
-
 var ml = moveList{}
+
+/////////////////////////////// 
+/*
+func (m move) pack(fr, to, cp, pr, ep, castl int) move {
+	packed := uint32(0)
+	packed = uint32(fr)
+	packed |= uint32(to)<<6  // fr needs 6 bits first
+	packed |= uint32(cp)<<(6+6) // fr+to needs 12 bits first
+	packed |= uint32(pr)<<(6+6+4) // cp needs 4 bits (with color = p12)
+	packed |= uint32(ep)<<(6+6+4+4) // pr needs 4 bits
+	packed |= uint32(castl)<<(6+6+4+4+6) // ep needs 6 bits ( and castl needs 4)
+	// we use 6+6+4+4+6+4 = 30  That means we have 32 bits left for move value 
+	return move(packed)
+}
+*/
